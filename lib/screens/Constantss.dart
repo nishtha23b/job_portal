c932c1342678a5ed4a8e144d1c738d18d12cfc20
ACER <paljeets.sambhi11@gmail.com>
2020-01-09 18:58:31 +0530

    Seeker options added.

---
+++ lib/screens/Constantss.dart
@@ -0,0 +1,14 @@
+class Constants{
+  static final String BaseURL = "https://arcane-eyrie-98677.herokuapp.com";
+  static final String LOGINPROVIDER_URL = BaseURL+"/login_employer/login_employer";
+  static final String LOGINSEEKER_URL = BaseURL+"/login_jobseeker/login_jobseeker";
+  static final String SIGNUPPROVIDER_URL = BaseURL+"/reg_employer/addemployer";
+  static final String SIGNUPSEEKER_URL = BaseURL+"/reg_jobseeker/addjobseeker";
+  static final String PROFILE = BaseURL+"/profile";
+
+  // SESSION PARAMETER
+
+  static final String LOGIN_STATUS="LOGIN_STATUS";
+  static final String USER_ID="USER_ID";
+
+}
